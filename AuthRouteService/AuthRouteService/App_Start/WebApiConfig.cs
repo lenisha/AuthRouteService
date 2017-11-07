04313c0b7e10e4e9198e3b72cbc481082a3e98a3
Elena Neroslavskaya <eneroslavskaya@pivotal.io>
2017-11-07 13:42:02 -0500

    .NET Route service POC using HTTPHandler

---
+++ AuthRouteService/AuthRouteService/App_Start/WebApiConfig.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Routing;
+
+namespace AuthRouteService
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+			// Web API configuration and services
+
+			// Web API routes
+			//config.MapHttpAttributeRoutes();
+
+			/*
+			config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+
+            ); */
+
+			config.MessageHandlers.Add(new ProxyHandler());
+
+			RouteTable.Routes.RouteExistingFiles = true;
+			RouteTable.Routes.MapHttpRoute("all", "{*path}");
+			
+			config.Routes.MapHttpRoute("allpath", "{*allpath}");
+		}
+    }
+}
---
+++ AuthRouteService/AuthRouteService/Models/ProxyHandler.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Web;
+using NLog;
+using System.Net;
+
+namespace AuthRouteService
+{
+	public class ProxyHandler : DelegatingHandler
+	{
+		public static  String FORWARDED_URL = "X-CF-Forwarded-Url";
+
+	    private static  String PROXY_METADATA = "X-CF-Proxy-Metadata";
+
+		private static  String PROXY_SIGNATURE = "X-CF-Proxy-Signature";
+
+		private static Logger logger = LogManager.GetCurrentClassLogger();
+
+		protected  override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+		{
+			return RedirectRequest(request, cancellationToken);
+		}
+
+		private async Task<HttpResponseMessage> RedirectRequest(HttpRequestMessage request, CancellationToken cancellationToken)
+		{
+			logger.Debug("Incoming Request {0} ", request);
+
+			String remoteserver = request.GetFirstHeaderValueOrDefault<String>(FORWARDED_URL);
+
+			if (remoteserver == null)
+				return await ErrorPage(request);
+
+		
+			ServicePointManager.ServerCertificateValidationCallback += (mender, certificate, chain, sslPolicyErrors) => true;
+
+			var client = new HttpClient(new HttpClientHandler {
+								AllowAutoRedirect = false,
+								UseCookies = false
+				
+			});
+
+
+			var remoteUri = new Uri(remoteserver);
+
+			var clonedRequest = await HttpRequestMessageExtensions.CloneHttpRequestMessageAsync(request, remoteUri);
+			logger.Debug("Forwarding to  {0} ", clonedRequest);
+
+			var httpResponseMessage = await client.SendAsync(clonedRequest, HttpCompletionOption.ResponseHeadersRead, cancellationToken);
+
+
+			logger.Debug("Outgoing Response {0} ", httpResponseMessage);
+
+			return httpResponseMessage;
+		}
+
+		private  Task<HttpResponseMessage> ErrorPage(HttpRequestMessage request)
+		{
+			// Create the response.
+			var response = new HttpResponseMessage(HttpStatusCode.ExpectationFailed)
+			{
+				Content = new StringContent("Route service does not get required constraints")
+			};
+
+			// Note: TaskCompletionSource creates a task that does not contain a delegate.
+			var tsc = new TaskCompletionSource<HttpResponseMessage>();
+			tsc.SetResult(response);   // Also sets the task state to "RanToCompletion"
+			return tsc.Task;
+		}
+	}
+}
